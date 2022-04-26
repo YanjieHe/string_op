41b3bee837dfe6e909820cb9769ba4f1aa8f19d9
Yanjie He <heyanjie0@outlook.com>
2022-04-26 16:39:42 -0400

    add a test case for string splitting

--- test/test_string_op.cpp
+++ test/test_string_op.cpp
@@ -8,6 +8,7 @@ TEST_CASE("Test String Operations", "[string_op]") {
   SECTION("Test Split") {
     REQUIRE(string_op::split<string>("abc,def,ghi", ",") ==
             vector<string>{"abc", "def", "ghi"});
+    REQUIRE(string_op::split<string>("abc", ",") == vector<string>{"abc"});
     REQUIRE(string_op::split<string>(",ONE,,TWO,,,THREE", ",") ==
             vector<string>{"", "ONE", "", "TWO", "", "", "THREE"});
   }
